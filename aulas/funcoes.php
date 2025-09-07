120c7db9cc14ec81bd1ce7f5734dbf68b94b80e8
Ricardo Uemura <uemuradev.exe@gmail.com>
2025-09-08 05:34:36 +0900

    parametros nomeados e tipados no php

--- aulas/funcoes.php
+++ aulas/funcoes.php
@@ -1,10 +1,17 @@
 <?php
 
-function saudacao (){
+function saudacao (string $nome){
     return "ola mundo";
-
+    
 }
 
-$oi = saudacao();
+$oi = saudacao("nome");
 
 echo $oi;
+
+function somar (int $n1, int $n2):string {
+    return $n1 + $n2;
+}
+
+echo somar($n1 = 17, $n2 = 70);
+
