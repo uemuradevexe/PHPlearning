797a92d2085ebfe427dc63d3532da759e7ffabfb
Ricardo Uemura <uemuradev.exe@gmail.com>
2025-09-08 06:19:08 +0900

    while e do while

---
+++ aulas/while.php
@@ -0,0 +1,13 @@
+<?php
+
+ $idade = 14;
+
+// while($idade <= 60){
+//     echo "sua idade e: $idade <br />";
+//     $idade++;
+// }
+
+do {
+    echo "to garantindo q isso vai acontecer";
+    $idade++;
+} while($idade <= 20);
