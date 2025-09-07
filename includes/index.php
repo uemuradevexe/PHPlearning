dd2b2e1a461853d51581596ec6edb129a17704e6
Ricardo Uemura <uemuradev.exe@gmail.com>
2025-09-07 12:49:33 +0900

    include e require

---
+++ includes/index.php
@@ -0,0 +1,19 @@
+<?php
+
+include "nome.php";
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inclusao de arquivos</title>
+</head>
+<body>
+    <h1>HEllo WOrld <?= $nome ?></h1>
+    
+</body>
+</html>
