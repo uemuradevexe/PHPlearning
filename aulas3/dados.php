3bb0b91bc63ab2cca2439adf90824bbeb9639287
Ricardo Uemura <uemuradev.exe@gmail.com>
2025-09-09 06:11:36 +0900

    lidando com XSS

--- aulas3/dados.php
+++ aulas3/dados.php
@@ -1,8 +1,8 @@
 <?php
 
 if(!empty($_POST['login']) && !empty($_POST['password'])){
-    $login = $_POST['login'];
-    $password = $_POST['password'];
+    $login = htmlspecialchars($_POST['login']);
+    $password = htmlspecialchars($_POST['password']);
 
     echo "Ola $login";
 }
